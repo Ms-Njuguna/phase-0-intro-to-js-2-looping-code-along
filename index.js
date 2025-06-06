c6f2af562122a19f249a95aa7c337796d2f791a3
Patricia <patricianjuguna084@gmail.com>
2025-06-06 17:36:23 +0300

    Did the looping lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solutions in this file
+//
+
+function  writeCards(names, eventName) {
+    let messages = [];
+
+    for(let i = 0; i < names.length; i++) {
+       messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);   
+    }
+    return messages;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+function countDown() {
+    let counter = 10;
+
+    while(counter >= 0) {
+        console.log(counter);
+        counter --;
+    }
+}
+
+countDown();
